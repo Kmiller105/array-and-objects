6f9a4b0039a9f23c56bdafba5691172c7d9de22d
Kimberly miller <135668982+Kmiller105@users.noreply.github.com>
2025-09-24 13:48:10 -0500

    update

--- arrayobject.js
+++ arrayobject.js
@@ -1,1 +1,29 @@
+const schedule =["wake up","eat","watch tv","sleep"];
+console.log(schedule[0]);
+console.log(schedule[1]);
+console.log(schedule[2]);
+console.log(schedule[3]);
+schedule[1]="brush teeth";
+console.log(schedule[1]);
+console.log(schedule,length);
+const user ={
+    name:"Mary",
+    age:29,
+    hobby:"sewing",
+    married:false,
+    purchase:["car","house","furniture","laptop"],
+sayName:function(){
+    console.log("my name is"+this.name);
+}
+ 
+};
+user.sayName();
+function saymyage(){
+    console.log("My age is"+user.age);
+
+}
+saymyage();
+
+
+
 
